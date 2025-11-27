f6b56a9c7d448d0ab054c5e21e6084e29afe1770
dhitozarfhan <dhitozarfhan@gmail.com>
2025-11-27 16:50:32 +0700

    membuat model siswa

---
+++ app/src/main/java/com/example/roomdatabase/viewmodel/EntyViewModel.kt
@@ -0,0 +1,19 @@
+package com.example.roomdatabase.viewmodel
+
+import com.example.roomdatabase.room.Siswa
+
+data class DetailSiswa(
+    val id: Int = 0,
+    val nama: String = "",
+    val alamat: String = "",
+    val telpon: String = "",
+)
+
+fun DetailSiswa.toSiswa(): Siswa = Siswa(
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    telpon = telpon
+)
+
+
