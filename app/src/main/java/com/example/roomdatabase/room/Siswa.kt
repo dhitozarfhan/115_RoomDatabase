1535f65ac23c65e224e8aaca581c6a68ea4cbd48
dhitozarfhan <dhitozarfhan@gmail.com>
2025-11-27 16:11:00 +0700

    menambahkan databasesiswa.kt di room

--- app/src/main/java/com/example/roomdatabase/room/Siswa.kt
+++ app/src/main/java/com/example/roomdatabase/room/Siswa.kt
@@ -3,7 +3,7 @@ package com.example.roomdatabase.room
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "tbl_siswa")
+@Entity(tableName = "tblSiswa")
 data class Siswa(
     @PrimaryKey(autoGenerate = true)
     val id: Int = 0,
