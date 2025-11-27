487f8bb5a994a668f986916163986951c9a85c34
dhitozarfhan <dhitozarfhan@gmail.com>
2025-11-27 21:20:16 +0700

    menambahkan halaman PenyediaViewModel.kt

---
+++ app/src/main/java/com/example/roomdatabase/viewmodel/PenyediaViewModel.kt
@@ -0,0 +1,30 @@
+package com.example.RoomDatabase.viewmodel.provider
+
+import android.app.Application
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.RoomDatabase.viewmodel.EntryViewModel
+import com.example.roomdatabase.repositori.AplikasiSiswa
+import com.example.roomdatabase.viewmodel.HomeViewModel
+
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer {
+            HomeViewModel(aplikasiSiswa().containerApp.repositoriSiswa)
+        }
+        initializer {
+            EntryViewModel(aplikasiSiswa().containerApp.repositoriSiswa)
+        }
+    }
+}
+
+/**
+ * Fungsi ekstensi query untuk objek [Application] dan mengembalikan sebuah
+ * instance dari
+ * [AplikasiSiswa].
+ */
+fun CreationExtras.aplikasiSiswa(): AplikasiSiswa =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiSiswa)
