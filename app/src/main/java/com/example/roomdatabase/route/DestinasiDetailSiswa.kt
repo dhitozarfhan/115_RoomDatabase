fcfe4d880a1011efe3dfeb40bbaaa6229db66909
dhitozarfhan <dhitozarfhan@gmail.com>
2025-12-04 16:53:47 +0700

    menambahkan destinasi detail siswa

---
+++ app/src/main/java/com/example/roomdatabase/route/DestinasiDetailSiswa.kt
@@ -0,0 +1,11 @@
+package com.example.roomdatabase.view.route
+
+import com.example.roomdatabase.R
+import com.example.roomdatabase.view.route.DestinasiNavigasi
+
+object DestinasiDetailSiswa : DestinasiNavigasi {
+    override val route = "detail_siswa"
+    override val titleRes = R.string.detail_siswa
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
