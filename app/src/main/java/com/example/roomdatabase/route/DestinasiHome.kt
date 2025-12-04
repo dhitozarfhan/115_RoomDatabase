89f39e3d857b485d0b1fc86e09c4fa803b093463
dhitozarfhan <dhitozarfhan@gmail.com>
2025-12-04 16:54:11 +0700

    memindahkan package route

--- app/src/main/java/com/example/roomdatabase/route/DestinasiHome.kt
+++ app/src/main/java/com/example/roomdatabase/route/DestinasiHome.kt
@@ -1,12 +1,10 @@
-package com.example.roomdatabase.route
+package com.example.roomdatabase.view.route
 
 import com.example.roomdatabase.R
-import com.example.roomdatabase.view.route.DestinasiNavigasi
 
+object DestinasiHome: DestinasiNavigasi {
 
-
-object DestinasiHome : DestinasiNavigasi {
     override val route = "home"
+
     override val titleRes = R.string.app_name
 }
-
