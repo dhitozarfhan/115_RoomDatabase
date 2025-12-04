0b32ae2ae364bcfd15677cbee64e472451a113d4
dhitozarfhan <dhitozarfhan@gmail.com>
2025-12-04 16:56:01 +0700

    menambahkan fungsi edit dan hapus

--- app/src/main/java/com/example/roomdatabase/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/roomdatabase/repositori/RepositoriSiswa.kt
@@ -1,5 +1,7 @@
 package com.example.roomdatabase.repositori
 
+
+import androidx.room.Dao
 import com.example.roomdatabase.room.Siswa
 import com.example.roomdatabase.room.SiswaDao
 import kotlinx.coroutines.flow.Flow
@@ -7,6 +9,11 @@ import kotlinx.coroutines.flow.Flow
 interface RepositoriSiswa {
     fun getAllSiswaStream(): Flow<List<Siswa>>
     suspend fun insertSiswa(siswa: Siswa)
+
+    fun getSiswaStream(id: Int): Flow<Siswa?>
+
+    suspend fun deleteSiswa(siswa: Siswa)
+    //suspend fun updateSiswa(siswa: Siswa)
 }
 
 class OfflineRepositoriSiswa(
@@ -14,4 +21,10 @@ class OfflineRepositoriSiswa(
 ) : RepositoriSiswa {
     override fun getAllSiswaStream(): Flow<List<Siswa>> = siswaDao.getAllSiswa()
     override suspend fun insertSiswa(siswa: Siswa) = siswaDao.insert(siswa)
+
+    //edit
+    override fun getSiswaStream(id: Int): Flow<Siswa?> = siswaDao.getSiswa(id)
+    override suspend fun deleteSiswa(siswa: Siswa) = siswaDao.delete(siswa)
+    //override suspend fun updateSiswa(siswa: Siswa) = siswaDao.update(siswa)
+
 }
